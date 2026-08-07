a03eb07f82f348f2e5146b4b1f490226810cd44a
bishal-bhattarai1 <bishalbhattarai221@gmail.com>
2026-08-07 18:07:17 +0545

    begineer question practise

---
+++ beginner_question_practise/prime_number.py
@@ -0,0 +1,18 @@
+#prime_number
+
+number = int(input("Enter the number: "))
+
+print ("\n ------ Number ---------- ")
+if number <= 1:
+    print(f"Give number:- {number} is not prime number.")
+else:
+    for i in range(2,number):
+        if number%i == 0:
+            print(f"Given number:- {number} is not prime number")
+            break
+    else:
+        print(f"Given number:- {number} is prime number.")
+
+
+print ("------ ----- ---------- ")
+
