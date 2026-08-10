634072077050130f488601ce46d5ed5c72ad53be
bishal-bhattarai1 <bishalbhattarai221@gmail.com>
2026-08-10 16:14:33 +0545

    Even and Odd Synatx added

--- beginner_question_practise/while_loop.py
+++ beginner_question_practise/while_loop.py
@@ -7,3 +7,23 @@ while i <= 10:  #condition
     print(i)
 
     i += 1 # update/change
+
+#######################################################################
+
+
+
+#Even and ODD Number
+num = 1
+
+even_number = []
+odd_number = []
+
+while num <= 20:
+    if num%2 == 0:
+        even_number.append(num)
+    else:
+        odd_number.append(num)
+    num += 1
+
+print("Even Number",even_number)
+print("Odd Number",odd_number)
