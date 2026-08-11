bf647617157ec7c94a18cc6fd25ee52b36cc2de9
bishal-bhattarai1 <bishalbhattarai221@gmail.com>
2026-08-11 16:54:44 +0545

    Length Calculation

--- beginner_question_practise/string_practise/q2_length.py
+++ beginner_question_practise/string_practise/q2_length.py
@@ -1,4 +1,5 @@
 # Store a sentence Python is very easy and print its length.
 
-sentence= input("Write some sentence: ")
+sentence= input("Write some sentence:   ")
+
 print(f"The length of sentence is",len(sentence),".")
