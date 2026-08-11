28b1299e16cc40c252a8aa8c5241fc73aa4dadd5
bishal-bhattarai1 <bishalbhattarai221@gmail.com>
2026-08-11 08:01:45 +0545

    Character output

---
+++ beginner_question_practise/string_practise/q3_character.py
@@ -0,0 +1,5 @@
+# Create a string word = programming and print the first and last character.
+
+word = "programing"
+print(f"The First Character of {word} is",word[0])
+print(f"The Last Character of {word} is", word[-1])
