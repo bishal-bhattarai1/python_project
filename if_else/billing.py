9955e257925b95abf55e7a032f09963017303359
bishal-bhattarai1 <bishalbhattarai221@gmail.com>
2026-08-13 17:41:59 +0545

    billing process

--- if_else/billing.py
+++ if_else/billing.py
@@ -35,3 +35,22 @@ else:
     print("Invalid membership status")
     exit()
 
+bill_after_discount = original_bill - discount
+
+vat = 0
+if bill_after_discount > 700:
+    vat = bill_after_discount * 0.13
+
+final_bill = bill_after_discount + vat
+
+
+
+print(f"Customer Name : {customer_name}")
+print(f"Membership : {membership.capitalize()}")
+print(f"Hours Used : {hours_used}")
+print(f"Time : {time.capitalize()}")
+print(f"Original Bill : Rs. {original_bill:.2f}") 
+print(f"Discount : Rs. {discount:.2f}")
+print(f"Bill After Discount: Rs. {bill_after_discount:.2f}") 
+print(f"VAT (13%) : Rs. {vat:.2f}")
+print(f"Final Bill : Rs. {final_bill:.2f}")
